294b3eb3d31d2802a8bbd830784a7d3943cebcea
James Cho <panamajim@gmail.com>
2019-09-05 12:53:24 -0700

    player metadata and clickhandlers for expand/collapse

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 class Gameboard {
-  constructor(numberOfPlayers) {
+  constructor() {
     this.players = [];
-    this.numberOfPlayers = numberOfPlayers;
     this.gemMine = null;
+    this.playerTurnDom = $(".currentPlayerContainer");
     this.playerTurnIndex = 0;
     this.mineGemClick = this.mineGemClick.bind(this);
     this.leaveMineClick = this.leaveMineClick.bind(this);
@@ -10,42 +10,27 @@ class Gameboard {
     this.typeOfGems = ["obsidian", "topaz", "amethyst", "emerald", "sapphire", "ruby", "diamond"];
     this.round = 1;
     this.domElements = {
-      // round number
-      // player turn
-      // player stats
-      // rules
     }
   }
 
   initializeBoard() {
-    // initialize game with all properties set to initial values
-    // create dom elements for game data
-    // create dom elements for players
-    // create appropriate number of players
-    // create mine
-    // start game with first player turn
     this.createMine();
     $(".buttonContainer").on("click", ".mineGems", this.mineGemClick);
     $(".buttonContainer").on("click", ".leaveMine", this.leaveMineClick);
     $(".rulesButton").on("click", function () { $(".rules").toggleClass("hidden") });
     $(".close-rules").on("click", function () { $(".rules").toggleClass("hidden") });
     $(".restart-game").on('click', this.restartGameClick);
+    $(".playerContainer").on('click', ".player", this.playerClick);
   }
 
   createPlayer(numberOfPlayers) {
-    // create new Player according to number of players
-    // append player dom elements
-    // var playerOne = $(".player1").addClass("yourTurn");
-    // this.players.push(new Player('.player1', 'Player 1'));
-    // var playerTwo = $(".player2");
-    // this.players.push(new Player('.player2', 'Player 2'));
-    for(var i = 1; i <= numberOfPlayers; i++) {
-        this.players.push(new Player('.player' + i, 'Player ' + i));
+    for(var playerIndex = 1; playerIndex <= numberOfPlayers; playerIndex++) {
+        this.players.push(new Player(playerIndex));
     }
+    this.cloneAndAppend();
   }
 
   createMine() {
-    // append mine dom elements
     this.gemMine = new Mine;
   }
 
@@ -59,7 +44,7 @@ class Gameboard {
       return;
     }
     this.updatePlayerGems(player, gemsMined);
-    this.updatePlayerPoints(player);
+    // this.updatePlayerPoints(player);
     this.nextPlayerTurn();
   }
 
@@ -67,7 +52,6 @@ class Gameboard {
     console.log("player " + (this.playerTurnIndex + 1) + " has left the mine");
     this.players[this.playerTurnIndex].leaveMine();
     this.currentPlayer().playerDom.toggleClass("leftMine");
-    //this.updatePlayerStatus( this.players[this.playerTurnIndex] );
     if (!this.checkIfEveryoneLeftMine()) {
       this.nextPlayerTurn();
     } else {
@@ -75,46 +59,60 @@ class Gameboard {
     }
   }
 
+  playerClick(){
+    if (!$(this).hasClass("expanded") && $(this).hasClass("collapsed")){
+      return;
+    }
+    var players = $(".player");
+    players.toggleClass("collapsed");
+    $(this).toggleClass("expanded");
+  }
+
   playerAccident(player) {
     player.hasAccident();
     player.playerDom.toggleClass("accident");
     player.returnGems(this.gemMine);
     this.updatePlayerGems(player, this.typeOfGems);
-    this.updatePlayerPoints(player);
     this.leaveMineClick();
   }
 
+  cloneAndAppend(){
+    this.playerTurnDom.empty();
+    this.currentPlayer().playerDom.clone().appendTo(this.playerTurnDom);
+    this.currentPlayer().playerDom.toggleClass("yourTurn");
+  }
+
   nextPlayerTurn() {
-    this.players[this.playerTurnIndex].playerDom.toggleClass("yourTurn");
+    this.currentPlayer().playerDom.toggleClass("yourTurn");
     if (this.playerTurnIndex === this.players.length - 1) {
       this.playerTurnIndex = 0;
     } else {
       this.playerTurnIndex++;
     }
-    this.players[this.playerTurnIndex].playerDom.toggleClass("yourTurn");
     var newPlayerTurnText = "Player Turn: Player " + (this.playerTurnIndex + 1);
     $(".playerTurn").text(newPlayerTurnText);
-    if (!this.players[this.playerTurnIndex].inMine) {
+    this.cloneAndAppend();
+    if (!this.currentPlayer().inMine) {
       this.nextPlayerTurn();
     }
-    console.log(this.players[this.playerTurnIndex].playerDom.attr("class"));
   }
 
   updatePlayerGems(player, newGems) {
     // update DOM to show player's current gem count
     for (var gemIndex = 0; gemIndex < newGems.length; gemIndex++) {
-      //var playerClass = $(player.playerDom).attr("class");
+      //player.updateGemCount(newGems[gemIndex], 1);
       var gemElement = $(player.playerDom).children("." + newGems[gemIndex]);
       var newGemCount = player.gems[newGems[gemIndex]];
       gemElement.text(newGems[gemIndex].charAt(0).toUpperCase() + newGems[gemIndex].slice(1) + ": " + newGemCount);
     }
   }
 
-  updatePlayerPoints(player) {
-    // update DOM to show player's current point total
-    var pointsElement = player.playerDom.children(".points");
-    pointsElement.text("Points: " + player.getPoints());
-  }
+  // updatePlayerPoints(player) {
+  //   // update DOM to show player's current point total
+
+  //   // var pointsElement = player.playerDom.children(".points");
+  //   // pointsElement.text("Points: " + player.getPoints());
+  // }
 
   checkIfEveryoneLeftMine() {
     for (var playerIndex = 0; playerIndex < this.players.length; playerIndex++) {
@@ -147,27 +145,12 @@ class Gameboard {
       this.players[playerIndex].returnGems(this.gemMine);
       this.updatePlayerGems(this.players[playerIndex], this.typeOfGems);
       this.players[playerIndex].playerDom.removeClass('accident yourTurn leftMine');
-      this.updatePlayerPoints(this.players[playerIndex]);
       this.players[playerIndex].inMine = true;
       this.players[playerIndex].hadAccident = false;
     }
-    this.players[0].playerDom.toggleClass('yourTurn');
     this.playerTurnIndex = 0;
     $('.winner').toggleClass('hidden');
+    this.cloneAndAppend();
   }
 
-  // updatePlayerStatus( player ){
-  //   // update player
-  //   //player.playerDom.addClass("accident");
-
-  // }
-
-  // updateBoard(){
-  //   // update dom elements to display the most current gem totals/player stats
-  // }
-
-  // playerTurn(){
-  //   // display screen for selected player's turn if player.inMine is true
-  //   // update board
-  // }
 }
