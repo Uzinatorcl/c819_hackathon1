bbc75cf9d73a618b22a3c1e52ae182a67a2768e7
James Cho <panamajim@gmail.com>
2019-09-08 00:32:49 -0700

    leader board, moved methods so player updates own dom

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
 class Gameboard {
   constructor() {
     this.players = [];
-    this.gemMine = null;
-    this.playerTurnDom = $(".currentPlayerContainer");
+    this.playersInMine = [];
+    this.gemMine = new Mine;
     this.playerTurnIndex = 0;
+    this.maxRounds = 5;
     this.mineGemClick = this.mineGemClick.bind(this);
     this.leaveMineClick = this.leaveMineClick.bind(this);
     this.restartGameClick = this.restartGameClick.bind(this);
@@ -14,6 +15,11 @@ class Gameboard {
     this.round = 0;
     this.audio;
     this.domElements = {
+      roundNumber: $(".roundNumber"),
+      playerTurnDom: $(".currentPlayerContainer"),
+      leaderBoard: $(".leaderBoard"),
+      leaderBoardPlayerNames: [],
+      leaderBoardPlayerPoints: [],
       gemModal: $(".gemsReceivedContainer"),
       gemModalPlayer: $(".playerMine"),
       gem1: $(".gem1"),
@@ -23,7 +29,6 @@ class Gameboard {
   }
 
   initializeBoard() {
-    this.createMine();
     this.audio = new Audio;
     $(".buttonContainer").on("click", ".mineGems", this.mineGemClick);
     $(".buttonContainer").on("click", ".leaveMine", this.leaveMineClick);
@@ -31,66 +36,82 @@ class Gameboard {
     $(".close-rules").on("click", function () { $(".rules").toggleClass("hidden") });
     $(".restart-game").on('click', this.restartGameClick);
     $('.createPlayer').on('click', this.createPlayer);
-    $('.playerContainer').on('click', '.player', this.playerClick);
+    $('.playerContainer').on('click', '.player', this.playerExpandClick);
     $('.volumeMute').on('click', this.muteVolumeClick);
-  }
-
-  roundChange() {
-    if(this.round <= 4) {
-    var currentRound = this.round.toString();
-    this.round++
-    var nextRound = this.round.toString();
-    var newRound = $('.roundNumber').text().replace(currentRound, nextRound);
-    $('.roundNumber').text(newRound);
-    } else {
-      this.round = 1;
-      $('.roundNumber').text($('.roundNumber').text().replace('5','1'))
-    }
   }
 
   createPlayer(event) {
     var numberOfPlayers = parseInt(event.currentTarget.innerText);
     $('.players-modal').toggleClass('hidden');
-    for(var i = 1; i <= numberOfPlayers; i++) {
-        this.players.push(new Player(i));
+    for(var playerIndex = 1; playerIndex <= numberOfPlayers; playerIndex++) {
+        this.players.push(new Player(playerIndex));
+        this.playersInMine.push(this.players[playerIndex - 1]);
     }
+    this.createLeaderBoard();
     this.roundChange();
-    this.cloneAndAppend();
+    this.createAndDisplayCurrentPlayer();
   }
 
-  createMine() {
-    this.gemMine = new Mine;
+  createLeaderBoard(){
+    for(var playerIndex = 0; playerIndex < this.players.length; playerIndex++){
+      var leaderBoardPlayerDom = $("<div>", { class: "leader" + (playerIndex + 1) });
+      var leaderBoardPlayerNameDom = $("<span>").text(this.players[playerIndex].getPlayerName() + ": ");
+      var leaderBoardPointsDom = $("<span>").text(this.players[playerIndex].getTotalPoints());
+      var leaderBoardPointUnitsDom = $("<span>").text("pts");
+      leaderBoardPlayerDom.append(leaderBoardPlayerNameDom, leaderBoardPointsDom, leaderBoardPointUnitsDom);
+      this.domElements.leaderBoard.append(leaderBoardPlayerDom);
+      this.domElements.leaderBoardPlayerNames.push(leaderBoardPlayerNameDom);
+      this.domElements.leaderBoardPlayerPoints.push(leaderBoardPointsDom);
+    }
   }
 
   mineGemClick() {
     this.clickSounds('mine.mp3');
-    console.log("player " + (this.playerTurnIndex + 1) + " is mining for gems");
     var player = this.currentPlayer();
-    var gemsMined = player.mine(this.gemMine);
-    this.updateGemsReceived(gemsMined);
-    if (player.gems["obsidian"] === 2) {
-      this.playerAccident(player);
+    var gemsMined = this.gemMine.mineTwoGems();
+    if (player.mine(gemsMined)){
+      this.playerAccident();
       return;
     }
-    this.updatePlayerGems(player, gemsMined);
+    this.showGemsReceivedMessage(gemsMined);
+    this.currentPlayer().getPlayerDom().removeClass("yourTurn");
     this.nextPlayerTurn();
   }
 
+  showGemsReceivedMessage(gems) {
+    this.domElements.gemModal.removeClass("hidden");
+    this.domElements.gemModalPlayer.text(this.currentPlayer().playerName + " Received:");
+    this.domElements.gem1.attr("class", "gem1 " + gems[0]);
+    this.domElements.gem1.html("&diams; " + gems[0]);
+    this.domElements.gem2.attr("class", "gem1 " + gems[1]);
+    this.domElements.gem2.html("&diams; " + gems[1]);
+    setTimeout(this.hideGemsReceivedModal, 500);
+  }
+
   leaveMineClick() {
-    if(this.currentPlayer().hadAccident === false) {
-      this.clickSounds('leave-mine.mp3');
-    }
-    console.log("player " + (this.playerTurnIndex + 1) + " has left the mine");
-    this.players[this.playerTurnIndex].leaveMine();
-    this.currentPlayer().playerDom.toggleClass("leftMine");
-    if (!this.checkIfEveryoneLeftMine()) {
+    this.clickSounds('leave-mine.mp3');
+    this.removePlayerFromMine();
+  }
+
+  playerAccident() {
+    this.clickSounds('accident.mp3');
+    this.currentPlayer().getPlayerDom().append($("<span>").addClass("accidentShade"));
+    this.removePlayerFromMine();
+  }
+
+  removePlayerFromMine(){
+    this.currentPlayer().leaveMine()
+    this.currentPlayer().getPlayerDom().removeClass("yourTurn");
+    this.playersInMine.splice(this.playerTurnIndex, 1);
+    if (this.playersInMine.length) {
+      this.playerTurnIndex--;
       this.nextPlayerTurn();
     } else {
       this.gameOver();
     }
   }
 
-  playerClick(){
+  playerExpandClick(){
     if (!$(this).hasClass("expanded") && $(this).hasClass("collapsed")){
       return;
     }
@@ -99,122 +120,111 @@ class Gameboard {
     $(this).toggleClass("expanded");
   }
 
-  playerAccident(player) {
-    this.clickSounds('accident.mp3');
-    player.hasAccident();
-    player.playerDom.toggleClass("accident accidentBackground");
-    player.returnGems(this.gemMine);
-    this.updatePlayerGems(player, this.typeOfGems);
-    this.leaveMineClick();
-  }
-
-  cloneAndAppend(){
-    this.playerTurnDom.empty();
-    this.currentPlayer().playerDom.clone().appendTo(this.playerTurnDom);
-    this.currentPlayer().playerDom.addClass("yourTurn");
+  createAndDisplayCurrentPlayer(){
+    this.domElements.playerTurnDom.empty();
+    this.currentPlayer().getPlayerDom().clone().appendTo(this.domElements.playerTurnDom);
+    this.currentPlayer().getPlayerDom().addClass("yourTurn");
   }
 
   nextPlayerTurn() {
-    this.currentPlayer().playerDom.removeClass("yourTurn");
-    if (this.playerTurnIndex === this.players.length - 1) {
+    if (this.playerTurnIndex === this.playersInMine.length - 1 || this.playerTurnIndex < 0) {
       this.playerTurnIndex = 0;
     } else {
       this.playerTurnIndex++;
     }
-    var newPlayerTurnText = "Player Turn: Player " + (this.playerTurnIndex + 1);
+    var newPlayerTurnText = "Player Turn: " + this.playersInMine[this.playerTurnIndex].getPlayerName();
     $(".playerTurn").text(newPlayerTurnText);
-    this.cloneAndAppend();
-    if (!this.currentPlayer().inMine) {
-      this.nextPlayerTurn();
-    }
-  }
-
-  updatePlayerGems(player, newGems) {
-    for (var gemIndex = 0; gemIndex < newGems.length; gemIndex++) {
-      var gemElement = $(player.playerDom).children("." + newGems[gemIndex]);
-      var newGemCount = player.gems[newGems[gemIndex]];
-      gemElement.text(newGems[gemIndex].charAt(0).toUpperCase() + newGems[gemIndex].slice(1) + ": " + newGemCount);
-    }
-  }
-
-  checkIfEveryoneLeftMine() {
-    for (var playerIndex = 0; playerIndex < this.players.length; playerIndex++) {
-      if (this.players[playerIndex].inMine === true) {
-        return false;
-      }
-    }
-    return true;
+    this.createAndDisplayCurrentPlayer();
   }
 
   currentPlayer() {
-    return this.players[this.playerTurnIndex];
+    return this.playersInMine[this.playerTurnIndex];
   }
 
   gameOver() {
     for(var playerIndex = 0; playerIndex < this.players.length; playerIndex++){
-      this.players[playerIndex].totalPoints += this.players[playerIndex].points;
-      this.players[playerIndex].pointsEachRound.push(this.players[playerIndex].points);
-      this.players[playerIndex].updateRoundPoints(this.round);
+      this.players[playerIndex].updatePointsAtEndOfRound(this.round);
     }
-    var winner = this.players[0];
-    var gameWinner = this.players[0];
-    for (var playerIndex = 1; playerIndex < this.players.length; playerIndex++) {
-      if (this.players[playerIndex].getPoints() > winner.getPoints()) {
-        winner = this.players[playerIndex];
+    if(this.round < this.maxRounds) {
+      this.updateLeaderBoard();
+      var roundWinner = this.players[0];
+      for (var playerIndex = 1; playerIndex < this.players.length; playerIndex++) {
+        if (this.players[playerIndex].getPoints() > roundWinner.getPoints()) {
+          roundWinner = this.players[playerIndex];
+        }
       }
-    }
-    // debugger;
-    if(this.round <= 4) {
-    $(".winningPlayer").text(winner.playerName);
+      $(".winningPlayer").text(roundWinner.getPlayerName());
       $('.winningMessage').text('You Win This Round!');
       $('.restart-game').text('Next Round');
-    $(".winner").toggleClass("hidden");
+      $(".winner").removeClass("hidden");
     } else {
+      var gameWinner = this.players[0];
       for (var playerIndex = 1; playerIndex < this.players.length; playerIndex++) {
         if (this.players[playerIndex].getTotalPoints() > gameWinner.getTotalPoints()) {
           gameWinner = this.players[playerIndex];
         }
       }
-      $(".winningPlayer").text(gameWinner.playerName);
-      $('.winningMessage').text('You Win! Your Total Points ' + gameWinner.getTotalPoints());
+      $(".winningPlayer").text(gameWinner.getPlayerName());
+      $('.winningMessage').text('You Win! Your Total Points: ' + gameWinner.getTotalPoints());
       $('.restart-game').text('Thanks for playing!');
-      $(".winner").toggleClass("hidden");
+      $(".winner").removeClass("hidden");
       for (var playerIndex = 0; playerIndex < this.players.length; playerIndex++) {
-        this.players[playerIndex].totalPoints = 0;
-        this.players[playerIndex].pointsEachRound = [];
-        this.players[playerIndex].domElements.roundPoints.text("");
+        this.players[playerIndex].initializePoints();
+        this.domElements.leaderBoardPlayerNames[playerIndex].text(this.players[playerIndex].getPlayerName() + ": ");
+        this.domElements.leaderBoardPlayerPoints[playerIndex].text(this.players[playerIndex].getTotalPoints());
       }
+    }
+  }
+
+  updateLeaderBoard() {
+    var leaders = [];
+    var copyOfPlayers = this.players.slice(0);
+    while (copyOfPlayers.length){
+      var highestScore = copyOfPlayers[0];
+      var highestIndex = 0;
+      for (var playerIndex = 0; playerIndex < copyOfPlayers.length; playerIndex++) {
+        if (highestScore.getTotalPoints() < copyOfPlayers[playerIndex].getTotalPoints()){
+          highestScore = copyOfPlayers[playerIndex];
+          highestIndex = playerIndex;
+        }
+      }
+      leaders.push(copyOfPlayers.splice(highestIndex,1)[0]);
+    }
+    for (var leaderIndex = 0; leaderIndex < leaders.length; leaderIndex++){
+      this.domElements.leaderBoardPlayerNames[leaderIndex].text(leaders[leaderIndex].getPlayerName() + ": ");
+      this.domElements.leaderBoardPlayerPoints[leaderIndex].text(leaders[leaderIndex].getTotalPoints());
     }
   }
 
   restartGameClick() {
     for (var playerIndex = 0; playerIndex < this.players.length; playerIndex++) {
-      this.players[playerIndex].returnGems(this.gemMine);
-      this.updatePlayerGems(this.players[playerIndex], this.typeOfGems);
-      this.players[playerIndex].playerDom.removeClass('accident yourTurn leftMine accidentBackground');
-      this.players[playerIndex].inMine = true;
-      this.players[playerIndex].hadAccident = false;
+      this.gemMine.returnPlayerGemsToMine(this.players[playerIndex].returnGems());
+      this.players[playerIndex].getPlayerDom().removeClass('accident yourTurn leftMine accidentBackground');
+      this.players[playerIndex].initializeStatus();
+      this.playersInMine.push(this.players[playerIndex]);
     }
-    this.playerTurnIndex = this.players.length - 1;
-    this.nextPlayerTurn();
+    this.playerTurnIndex = 0
+    this.playersInMine[0].getPlayerDom().addClass("yourTurn");
     this.roundChange();
-    $('.winner').toggleClass('hidden');
-    this.cloneAndAppend();
-}
-
-  updateGemsReceived(gems){
-    console.log(gems);
-    this.domElements.gemModal.toggleClass("hidden");
-    this.domElements.gemModalPlayer.text(this.currentPlayer().playerName + " Received:");
-    this.domElements.gem1.attr("class", "gem1 " + gems[0]);
-    this.domElements.gem1.html("&diams; " + gems[0]);
-    this.domElements.gem2.attr("class", "gem1 " + gems[1]);
-    this.domElements.gem2.html("&diams; " + gems[1]);
-    setTimeout(this.hideGemsReceivedModal, 1200);
+    $('.winner').addClass('hidden');
+    this.createAndDisplayCurrentPlayer();
   }
 
   hideGemsReceivedModal(){
-    this.domElements.gemModal.toggleClass("hidden");
+    this.domElements.gemModal.addClass("hidden");
+  }
+
+  roundChange() {
+    if (this.round < this.maxRounds) {
+      var currentRound = this.round.toString();
+      this.round++
+      var nextRound = this.round.toString();
+      var newRound = this.domElements.roundNumber.text().replace(currentRound, nextRound);
+      this.domElements.roundNumber.text(newRound);
+    } else {
+      this.round = 1;
+      this.domElements.roundNumber.text(this.domElements.roundNumber.text().replace('5', '1'))
+    }
   }
 
   clickSounds(fileName) {
@@ -229,6 +239,6 @@ class Gameboard {
       this.domElements.volumeMute.text('ON');
       this.audio.volume = 1;
     }
-  }
+   }
 
 }
